c25da8fe96d14fa40efb1f523016ca9c2c4ef3ad
Katie Keel <akatiekeel@gmail.com>
2017-10-13 11:08:29 -0600

    Initial setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :populate_tree
+  helper_method :tree
+
+  def populate_tree
+    @tree = CompleteMe.new
+  end
 end
